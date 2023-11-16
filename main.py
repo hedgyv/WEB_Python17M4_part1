c0c0c289e1b4833d924313ee6d7ee84705670c8b
hedgyv <hedgy85@hotmail.com>
2023-11-16 18:44:12 +0200

    WEB_M04_main_task

--- main.py
+++ main.py
@@ -1,11 +1,23 @@
 import mimetypes
 import urllib.parse
+import json
 from pathlib import Path
 from http.server import HTTPServer, BaseHTTPRequestHandler
+from threading import Thread
+import socket
+import logging
+from datetime import datetime
 
-from jinja2 import Environment, FileSystemLoader
+#from jinja2 import Environment, FileSystemLoader
 
 BASE_DIR = Path()
+BUFFER_SIZE = 1024
+HTTP_PORT = 3000
+HTTP_HOST = '0.0.0.0'
+SOCKET_HOST = '127.0.0.1'
+SOCKET_PORT = 5000
+
+#jinja = Environment(loader=FileSystemLoader('templates'))
 
 class MyFirstWEBServer(BaseHTTPRequestHandler):
     
@@ -15,8 +27,9 @@ class MyFirstWEBServer(BaseHTTPRequestHandler):
         match route.path:
             case '/':
                 self.send_html('index.html')
-            case '/contact':
-                self.send_html('contact.html')
+            case '/message':
+                self.send_html('message.html')
+                #self.render_template('message.html')
             case _:
                 file = BASE_DIR.joinpath(route.path[1:])
                 if file.exists():
@@ -25,7 +38,16 @@ class MyFirstWEBServer(BaseHTTPRequestHandler):
                     self.send_html('error.html', 404)
             
     def do_POST(self):
-        pass
+        size = self.headers.get("Content-Length")
+        data = self.rfile.read(int(size))
+        
+        client_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        client_socket.sendto(data, (SOCKET_HOST, SOCKET_PORT))
+        client_socket.close()
+        
+        self.send_response(302)
+        self.send_header('Location', '/message')
+        self.end_headers()
     
     def send_html(self, filename, status_code=200):
         self.send_response(status_code)
@@ -33,6 +55,13 @@ class MyFirstWEBServer(BaseHTTPRequestHandler):
         self.end_headers()
         with open(filename, 'rb') as file:
             self.wfile.write(file.read())
+            
+    # def render_template(self, filename, status_code=200):
+    #     self.send_response(status_code)
+    #     self.send_header('Content-Type', 'text/html')
+    #     self.end_headers()
+    #     with open(filename, 'rb') as file:
+    #         self.wfile.write(file.read())
             
     def send_static(self, filename, status_code=200):
         self.send_response(status_code)
@@ -44,10 +73,37 @@ class MyFirstWEBServer(BaseHTTPRequestHandler):
         self.end_headers()
         with open(filename, 'rb') as file:
             self.wfile.write(file.read())
+            
+
+def save_data_from_form(data):
+    parse_data = urllib.parse.unquote_plus(data.decode())
+    try:
+        parse_dict = {str(datetime.now()):{key: value for key, value in [el.split('=') for el in parse_data.split('&')]}}
+        with open('storage/data.json', 'w', encoding='utf-8') as file:
+            json.dump(parse_dict, file, ensure_ascii=False, indent=4)
+    except ValueError as err:
+        logging.error(err)
+    except OSError as err:
+        logging.error(err)
+        
+def run_socket_server(host, port):
+    server_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    server_socket.bind((host, port))
+    logging.info("Starting socket server")
+    try:
+        while True:
+            msg, address = server_socket.recvfrom(BUFFER_SIZE)
+            logging.info(f"Socket received {address}: {msg}")
+            save_data_from_form(msg)
+    except KeyboardInterrupt:
+        pass
+    finally:
+        server_socket.close()
     
-def run_server():
-    address = ('localhost', 3000)    
+def run_http_server(host, port):
+    address = (host, port)    
     http_server = HTTPServer(address, MyFirstWEBServer)
+    logging.info("Starting http server")
     try:
         http_server.serve_forever()
     except KeyboardInterrupt:
@@ -57,6 +113,12 @@ def run_server():
         
         
 if __name__ == '__main__':
-    run_server()    
+    logging.basicConfig(level=logging.DEBUG, format='%(threadName)s %(message)s')
+
+    server = Thread(target=run_http_server, args=(HTTP_HOST, HTTP_PORT))
+    server.start()
+
+    server_socket = Thread(target=run_socket_server, args=(SOCKET_HOST, SOCKET_PORT))
+    server_socket.start()    
     
         
